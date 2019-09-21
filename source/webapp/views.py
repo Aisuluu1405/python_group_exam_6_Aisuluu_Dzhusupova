c86180b2fd1096f37ae8e2ce60eb5d8a14e5a75e
Айсулу <aisuluueco2009@yandex.ru>
2019-09-21 13:53:11 +0600

    "Create index page(url, veiws,style.css)"

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+# from webapp.forms import ProductForm
+from webapp.models import Register
 
-# Create your views here.
+
+def index_view(request, *args, **kwargs):
+    registers = Register.objects.filter(status='active').order_by('-create')
+    return render(request, 'index.html', context={
+        'registers': registers
+    })
+
+
