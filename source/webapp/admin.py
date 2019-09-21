7bb506bf2e5be430490f838eadee1a6e09c97980
Айсулу <aisuluueco2009@yandex.ru>
2019-09-21 12:25:22 +0600

    Create model 'Register',create migrations, change admin.py and add fixtures

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from webapp.models import Register
 
-# Register your models here.
+
+class RegisterAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'author', 'text', 'status', 'create', 'update']
+    list_filter = ['status']
+    list_display_links = ['author']
+    search_fields = ['author']
+    exclude = []
+
+
+admin.site.register(Register, RegisterAdmin)
+
+
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+STATUS_CHOICES = (
+    ('active', 'Active'),
+    ('blocked', 'Blocked'),
+)
+
+
+class Register(models.Model):
+    author = models.CharField(max_length=50, verbose_name='Author')
+    email = models.EmailField(verbose_name='Email')
+    text = models.TextField(max_length=3000, verbose_name='Text')
+    create = models.DateTimeField(auto_now_add=True, verbose_name='Create date')
+    update = models.DateTimeField(auto_now=True, verbose_name='Change date')
+    status = models.CharField(max_length=20, null=False, blank=False, verbose_name='Status',
+                              default=STATUS_CHOICES[0][0],
+                              choices=STATUS_CHOICES)
+
+    def __str__(self):
+        return self.author
+
